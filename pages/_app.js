4debc7cd8dd132958ca90434cc51253d9c137369
Mariano De Simone <desimone.mariano@gmail.com>
2023-03-14 16:33:29 +0100

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-GLSQNX4ZJD"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || []; 
+        function gtag() {dataLayer.push(arguments);}
+        gtag('js', new Date()); 
+        gtag('config', 'G-GLSQNX4ZJD'), {
+        page_path: window.location.pathname,
+        };
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
